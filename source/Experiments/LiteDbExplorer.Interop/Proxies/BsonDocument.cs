1d90d57f6f93d4519cffeb031f3d2dc744a9b0e5
Julian Paulozzi <julianpaulozzi@gmail.com>
2020-02-21 18:16:49 -0300

    Split project in solutions
    - Split project in solutions to not compile unnecessary parts.
    - Created local only Nuget packages for 'Others' projects.

---
+++ source/Experiments/LiteDbExplorer.Interop/Proxies/BsonDocument.cs
@@ -0,0 +1,42 @@
+﻿extern alias v4;
+extern alias v5;
+using LiteDBv4 = v4::LiteDB;
+using LiteDBv5 = v5::LiteDB;
+
+namespace LiteDbExplorer.Interop
+{
+    
+
+    public interface IBsonDocumentProxy : IBsonValueProxy// , IDictionary<string, IBsonValueProxy>
+    {
+        // IEnumerable<IBsonValueProxy> Get(string path, bool includeNullIfEmpty);
+        // bool Set(string path, LiteDBv5.BsonExpression expr);
+        // bool Set(string path, LiteDBv5.BsonValue value);
+        // bool Set(string path, LiteDBv5.BsonExpression expr, bool addInArray);
+        // bool Set(string path, IBsonValueProxy value, bool addInArray);
+        
+        // void CopyTo(IBsonDocumentProxy doc);
+        // Dictionary<string, IBsonValueProxy> RawValue { get; }
+    }
+
+    public class BsonDocumentProxyV4 : BsonValueProxyV4, IBsonDocumentProxy
+    {
+        private readonly LiteDBv4.BsonDocument _bsonDocument;
+
+        public BsonDocumentProxyV4(LiteDBv4.BsonDocument bsonDocument) : base(bsonDocument)
+        {
+            _bsonDocument = bsonDocument;
+        }
+    }
+
+    public class BsonDocumentProxyV5 : BsonValueProxyV5, IBsonDocumentProxy
+    {
+        private readonly LiteDBv5.BsonDocument _bsonDocument;
+
+        public BsonDocumentProxyV5(LiteDBv5.BsonDocument bsonDocument) : base(bsonDocument)
+        {
+            _bsonDocument = bsonDocument;
+        }
+    }
+
+}
---
+++ source/Experiments/LiteDbExplorer.Interop/Proxies/LiteDatabase.cs
@@ -0,0 +1,51 @@
+﻿extern alias v4;
+extern alias v5;
+using System.Collections.Generic;
+using System.IO;
+using LiteDBv4 = v4::LiteDB;
+using LiteDBv5 = v5::LiteDB;
+
+namespace LiteDbExplorer.Interop
+{
+    public interface ILiteDatabaseProxy
+    {
+        LiteDBv5.ILiteCollection<T> GetCollection<T>(string name);
+        LiteDBv5.ILiteCollection<T> GetCollection<T>();
+        LiteDBv5.ILiteCollection<T> GetCollection<T>(LiteDBv5.BsonAutoId autoId);
+        LiteDBv5.ILiteCollection<LiteDBv5.BsonDocument> GetCollection(string name, LiteDBv5.BsonAutoId autoId);
+        bool BeginTrans();
+        bool Commit();
+        bool Rollback();
+        LiteDBv5.ILiteStorage<TFileId> GetStorage<TFileId>(string filesCollection, string chunksCollection);
+        IEnumerable<string> GetCollectionNames();
+        bool CollectionExists(string name);
+        bool DropCollection(string name);
+        bool RenameCollection(string oldName, string newName);
+        LiteDBv5.IBsonDataReader Execute(TextReader commandReader, LiteDBv5.BsonDocument parameters);
+        LiteDBv5.IBsonDataReader Execute(string command, LiteDBv5.BsonDocument parameters);
+        LiteDBv5.IBsonDataReader Execute(string command, params LiteDBv5.BsonValue[] args);
+        void Checkpoint();
+        long Rebuild(LiteDBv5.Engine.RebuildOptions options);
+        // BsonValue Pragma(string name);
+        // BsonValue Pragma(string name, BsonValue value);
+        void Dispose();
+        // BsonMapper Mapper { get; }
+        LiteDBv5.ILiteStorage<string> FileStorage { get; }
+        int UserVersion { get; set; }
+    }
+
+    public class LiteDatabaseProxyV5 : LiteDBv5.LiteDatabase, ILiteDatabaseProxy
+    {
+        public LiteDatabaseProxyV5(string connectionString, LiteDBv5.BsonMapper mapper = null) : base(connectionString, mapper)
+        {
+        }
+
+        public LiteDatabaseProxyV5(LiteDBv5.ConnectionString connectionString, LiteDBv5.BsonMapper mapper = null) : base(connectionString, mapper)
+        {
+        }
+
+        public LiteDatabaseProxyV5(Stream stream, LiteDBv5.BsonMapper mapper = null) : base(stream, mapper)
+        {
+        }
+    }
+}
